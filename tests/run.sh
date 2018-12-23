7aa3e64b3a03f93336681cbaeae665b3af25200f
Chris MacNaughton <chris@centaurisolutions.nl>
2018-12-23 22:04:46 +0100

    another one

--- tests/run.sh
+++ tests/run.sh
@@ -28,6 +28,10 @@ then
 fi
 lxc launch ubuntu:xenial master
 sleep 5
+lxc profile show default
+lxc profile unset default core.proxy_http
+lxc profile unset default user.network_mode
+lxc profile show default
 master_ip=`lxc list "master" -c 4 | awk '!/IPV4/{ if ( $2 != "" ) print $2}'`
 export master_ip
 if [ -n ${VERBOSE+x} ];
